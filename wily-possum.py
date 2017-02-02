90fbf8517d8d3e345a617044c5f7aa943a60a173
Clay Wells <cwells@cwells.org>
2017-02-02 10:20:00 -0500

    adding wily-possum.py script.

---
+++ wily-possum.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+
+'''
+This will run the firewall tests.
+TODO: should we call submodules from here as well?
+
+What capabilities of the firewall do we want to test?
+
+- what gets blocked?
+- what gets through?
+- check for TLS MiTM
+
+Options for the various tests:
+
+'''
+
+# ######### IMPORTS #########
+
+
+# ######### VARIABLES #########
+
+
+# ######### FUNCTIONS #########
+
+# Display a cool hacker banner at start-up.
+def display_banner():
+    print 'TODO: display banner'
+
+
+
+# ######### MAIN PROGRAM #########
+def main():
+    print 'TODO: main()'
+    display_banner()
+
+
+if __name__ == '__main__':
+    main()
