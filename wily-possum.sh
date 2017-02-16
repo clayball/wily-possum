9e393a98eb03f1b63575cc9a1f64c38803cf7ce4
Clay Wells <cwells@cwells.org>
2017-02-15 23:16:04 -0500

    lots of stuff... port option $2, added total hops, open/closed/filtered.
    added some ttl logic for determining the total hop count.
    added results for SA flag and some info for filtered state.

--- wily-possum.sh
+++ wily-possum.sh
@@ -23,6 +23,7 @@
 #
 
 DEST=$1
+PORT=$2
 
 # ######### NOTES #########
 #
@@ -37,35 +38,56 @@ DEST=$1
 # TODO: add a description for each test
 function test_tcp_syn()
 {
-    printf %b "[TEST_TCP_SYN]\n"
-    result=`nping -c 1 -H --tcp -p 80 --seq 31337 --ttl 64 --flags SYN $DEST | grep RCVD | awk {'print $7, $8, $9, $12'}`
+    printf %b "[TEST_TCP_SYN] $DEST:$PORT\n"
+    result=`nping -c 1 -H --tcp -p $PORT --seq 31337 --ttl 64 --flags SYN $DEST | grep RCVD | awk {'print $7, $8, $9, $12, $15'}`
     printf %b "\t[result] $result\n"
     flag=`echo $result | awk {'print $1'}`
-    ttl=`echo $result | awk {'print $2'}`
+    if [ "$flag" = "RA" ] || [ "$flag" = "SA" ]
+    then
+        echo "FLAG: $flag"
+        ttlstr=`echo $result | awk {'print $2'}`
+        ttl=`echo ${ttlstr:4:5}`  # only works for RA and SA flags
+        hops=$((64-$ttl))
+        # move this out when implementing type 3 code 10 condition
+        # TODO: should we do this for every test case or break it out into its own function?
+        printf %b "\t[ttl]    $ttl\n"
+        printf %b "\t[hops]   $hops\n"
+    fi
     if [ "$flag" == "RA" ]
     then
-        printf %b "\t[flag] Reset Ack\n"
+        printf %b "\t[flag]   Reset Ack\n"
+        printf %b "\t[state]  CLOSED\n"
+    elif [ "$flag" == "SA" ]
+    then
+        printf %b "\t[flag]   Syn-Ack\n"
+        printf %b "\t[state]  OPEN - w00t! w00t!\n"
+    elif [ "$flag" == "Destination" ]
+    then
+        restype=`echo $result | awk {'print $4'}`
+        printf %b "\t[info]     Host FW rule likely in place: REJECT --reject-with icmp-net-prohibited\n"
+        printf %b "\t[response] $restype\n"
+        printf %b "\t[state]    FILTERED\n"
     else
-        printf %b "\t[flag] $flag\n"
+        printf %b "\t[flag]   $flag\n"
     fi
-    printf %b "\t[ttl] $ttl\n"
+
     printf %b "__________________________________________________\n"
 }
 
 function test_tcp_ack()
 {
-    printf %b "[TEST_TCP_ACK]\n"
-    result=`nping -c 1 -H --tcp -p 80 --seq 31337 --ttl 64 --flags ACK $DEST | grep RCVD | awk {'print $7, $8, $9, $12'}`
+    printf %b "[TEST_TCP_ACK] $DEST:$PORT\n"
+    result=`nping -c 1 -H --tcp -p $PORT --seq 31337 --ttl 64 --flags ACK $DEST | grep RCVD | awk {'print $7, $8, $9, $12'}`
     printf %b "\t[result] $result\n"
     flag=`echo $result | awk {'print $1'}`
-    ttl=`echo $result | awk {'print $2'}`
+    ttlstr=`echo $result | awk {'print $2'}`
     if [ "$flag" == "R" ]
     then
         printf %b "\t[flag] Reset\n"
     else
         printf %b "\t[flag] $flag\n"
     fi
-    printf %b "\t[ttl] $ttl\n"
+    printf %b "\t[ttl] $ttlstr\n"
     printf %b "__________________________________________________\n"
 }
 
@@ -82,8 +104,8 @@ printf %b " \ V  V / | | || | | '_ \) _ (_-<_-< || | '  \()\n"
 printf %b "  \_/\_/|_|_|\_, | | .__/\___/__/__/\_,_|_|_|_|\n"
 printf %b "             |__/  |_|\n"
 printf %b "\n"
-printf %b " Firewall testing tool suite\n\n"
-printf %b "__________________________________________________\n\n"
+printf %b " Firewall Testing Tool Suite\n\n"
+printf %b "__________________________________________________\n"
 
 test_tcp_syn
-test_tcp_ack
+#test_tcp_ack
