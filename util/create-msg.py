232b503770e4d8c5ba26371f5f03f0623bac5791
ꓛ㇜ay <cwells@cwells.org>
2017-04-28 19:34:52 -0400

    minor update to create-msg.py

--- util/create-msg.py
+++ util/create-msg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Create the return an integer representing the Unicode code point of the
-# character
+# character.
 # c is each character of the message to encode.
 # m is the encoded character to send.
 
